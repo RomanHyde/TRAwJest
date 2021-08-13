19e86688239ed30c1a1212d8d9bd35a992d49015
RomanHyde <romanhyde1@gmail.com>
2021-08-13 18:19:45 +0100

    set up and tear down notes

---
+++ src/components/QuestionList.spec.js
@@ -0,0 +1,20 @@
+describe("The question list", () => {
+    // Runs before each test in a describe block
+    beforeEach(() => {
+        console.log("Before each!")
+    });
+
+    // Only runs once before all tests in a describe block
+    // beforeAll will execute first regardless of placement in realtion to beforeEach
+    beforeAll(() => {
+        console.log("Before all!")
+    });
+
+    it ("should display a list of items", () => {
+        expect(40+2).toEqual(42);
+    });
+
+    it ("should be the meaning of life", () => {
+        expect(40+2).toEqual(42);
+    });
+});
